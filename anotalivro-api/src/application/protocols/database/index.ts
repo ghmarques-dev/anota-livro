dce1696132c903cd3c1737a544d91b3cf1e120c4
Guilherme Marques <guilhermehenrique12308@gmail.com>
2025-05-27 20:26:07 -0300

    feat: add notes repository contract

--- anotalivro-api/src/application/protocols/database/index.ts
+++ anotalivro-api/src/application/protocols/database/index.ts
@@ -1,2 +1,3 @@
 export * from './users-repository';
 export * from './books-repository'
+export * from './notes-repository'
---
+++ anotalivro-api/src/application/protocols/database/notes-repository.ts
@@ -0,0 +1,81 @@
+import type { Note } from 'src/domain/entities';
+
+export abstract class NotesRepository {
+  abstract create(
+    input: NotesRepository.Create.Input,
+  ): NotesRepository.Create.Output;
+
+  abstract find_by_id(
+    input: NotesRepository.FindById.Input,
+  ): NotesRepository.FindById.Output;
+
+  abstract list_by_book(
+    input: NotesRepository.ListByBook.Input,
+  ): NotesRepository.ListByBook.Output;
+
+  abstract update(
+    input: NotesRepository.Update.Input,
+  ): NotesRepository.Update.Output;
+
+  abstract delete(
+    input: NotesRepository.Delete.Input,
+  ): NotesRepository.Delete.Output;
+}
+
+export namespace NotesRepository {
+  export namespace Create {
+    export type Input = {
+      note_id?: string;
+      book_id: string;
+      page?: number;
+      title: string;
+      content: string;
+    };
+
+    export type Output = Promise<Note>;
+  }
+
+  export namespace FindById {
+    export type Input = {
+      note_id: string;
+      book_id: string;
+    };
+
+    export type Output = Promise<Note | null>;
+  }
+
+  export namespace ListByBook {
+    export type Input = {
+      book_id: string;
+      page?: number;
+      limit?: number;
+      order_by?: 'created_at' | 'updated_at';
+      order_direction?: 'asc' | 'desc';
+    };
+
+    export type Output = Promise<Note[]>;
+  }
+
+  export namespace Update {
+    export type Input = {
+      note_id: string;
+      book_id: string;
+      data: Partial<{
+        title: string;
+        content: string;
+        tags: string[];
+      }>;
+    };
+
+    export type Output = Promise<Note | null>;
+  }
+
+  export namespace Delete {
+    export type Input = {
+      note_id: string;
+      book_id: string;
+    };
+
+    export type Output = Promise<void>;
+  }
+}
